034ebdbfe3011e95d3cd3e4ffe05cb4842daae6a
rohityadav-me <rohit.yadav102@gmail.com>
2026-02-27 03:07:18 +0530

    UI code added

---
+++ tip-jar/app/frontend/components/WalletProvider.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider
+} from "@solana/wallet-adapter-react";
+
+import {
+  WalletModalProvider
+} from "@solana/wallet-adapter-react-ui";
+
+import {
+  PhantomWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+export default function SolanaProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  const endpoint = "https://api.devnet.solana.com";
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter()],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
